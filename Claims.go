b33847a42fb1856d4a221eab8d475c6ea6f65129
David Bigelow <david.bigelow@pdgc.com>
2020-03-25 12:43:48 -0700

    Making One File Per Entity

--- Claims.go
+++ Claims.go
@@ -1,1 +1,13 @@
 package entity
+
+import "gopkg.in/dgrijalva/jwt-go.v3"
+
+// This is custom claims to to pass around all the role information tied to the user when the user authenticates
+//
+type Claims struct {
+	UserName string `json:"username"`
+	UserId  uint    `json:"userid"`
+	ProfileId uint   `json:"profileid"`
+	Roles map[string]bool  `json:"rolemap"`
+	jwt.StandardClaims
+}
--- Credentials.go
+++ Credentials.go
@@ -1,1 +1,10 @@
 package entity
+
+
+// Create a struct that models the structure of a user, both in the request body, and in the DB
+// This is the basic structure that will be used
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
