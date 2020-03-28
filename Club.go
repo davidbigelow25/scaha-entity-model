173268a820831ade0b9acd65a44336253c8f5d95
David Bigelow <david.bigelow@pdgc.com>
2020-03-28 10:44:02 -0700

    Adding the Club Entity to get a list of clubs

--- Club.go
+++ Club.go
@@ -1,1 +1,23 @@
 package entity
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+//
+// This represents a Club
+//
+// ToDo:  Adding all the staff Relationships
+//
+// ToDo: Add all the teams Here
+
+type Club struct {
+	gorm.Model
+	CahaNumber    string
+	Tag  		  string
+	ShortName 	  string
+	Name 		  string
+	IsHighSchool  bool
+	MobileImageURL string
+}
+
