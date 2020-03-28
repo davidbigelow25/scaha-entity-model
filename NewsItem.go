00349bfa8c82ff71eee85b14f34f8eebef73a424
David Bigelow <david.bigelow@pdgc.com>
2020-03-28 11:32:41 -0700

    NewsItemEntity

--- NewsItem.go
+++ NewsItem.go
@@ -1,2 +1,14 @@
 package entity
 
+import "github.com/jinzhu/gorm"
+
+type NewsItem struct {
+	gorm.Model
+	Title    string
+	Author   string
+	Subject  string
+	Intro    string
+	Body     string
+	State    string
+	IsActive bool
+}
