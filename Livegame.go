b8118ca380821dfbbe3df9bfa99f0da67ad5a4e9
David Bigelow <david.bigelow@pdgc.com>
2020-03-29 15:34:01 -0700

    Some more changes

--- Livegame.go
+++ Livegame.go
@@ -1,1 +1,38 @@
 package entity
+
+import (
+	"time"
+)
+
+//
+// This represents a Club
+//
+// ToDo:  Adding all the staff Relationships
+//
+// ToDo: Add all the teams Here
+
+type Livegame struct {
+
+	IdTeamHome	   uint  `gorm:"column:idteamhome"`
+	IdTeamAway	   uint   `gorm:"column:idteamhome"`
+	HomeTeam	  Teams    `gorm:"foreignkey:IdTeamHome"`
+	AwayTeam	  Teams    `gorm:"foreignkey:IdTeamAway"`
+
+	Idschedule 	   uint
+	Typetag			string
+	Statetag       string
+	Venuetag       string
+	Sheetname      string
+	Actdate		   time.Time
+	Starttime      time.Time
+	Gametag        string
+	Scorehome      uint
+	Scoreaway      uint
+	Gamenotes      string
+
+	ID     uint `gorm:"column:idlivegame;primary_key"`
+	UpdatedAt 	   time.Time `gorm:"column:updated"`
+	DeletedAt     *time.Time `sql:"index"`
+	CreatedAt 	   time.Time
+}
+
--- Team.go
+++ Team.go
@@ -1,1 +1,26 @@
 package entity
+
+import "time"
+
+type Teams struct {
+	Idclub  uint
+	Sname string
+	Teamname string
+	Teamgender string
+	Idskilllevel uint
+	Iddivisions uint
+	Divisiontag string
+	Isexhibition bool
+	Year 	  uint
+	Seasontag string
+	Scheduletags  string
+	Grouptag string
+	PlayerCountCalc uint `gorm:"column:playercountcalc"`
+
+	// Controll information
+	ID uint `gorm:"column:idteams;primary_key"`
+	IsActive bool    `gorm:"column:isactive"`
+	UpdatedAt  time.Time `gorm:"column:updated"`
+	DeletedAt  *time.Time `sql:"index"`
+	CreatedAt  time.Time
+}
