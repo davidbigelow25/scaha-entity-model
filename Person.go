ec868685ac841cf24a93fd818609612bd4e11368
David Bigelow <david.bigelow@pdgc.com>
2020-03-29 18:32:44 -0700

    Fixing Person to use old structure

--- Person.go
+++ Person.go
@@ -2,32 +2,37 @@ package entity
 
 import (
 	"errors"
-	"github.com/jinzhu/gorm"
 	"html"
 	"strings"
 	"time"
 )
 
 type Person struct {
-	gorm.Model
-	FirstName        string `gorm:"size:45;not null"`
-	LastName         string `gorm:"size:45;not null"`
+	FirstName        string `gorm:"size:45;column:fname;not null"`
+	LastName         string `gorm:"size:45;column:lname;not null"`
 	Email            string `gorm:"size:45;not null"`
 	Phone            string `gorm:"size:14"`
-	Adress           string `gorm:"size:75"`
+	Address          string `gorm:"size:75"`
 	City             string `gorm:"size:45"`
 	State            string `gorm:"size:2"`
 	Zipcode          int
 	Country          string `gorm:"size:45"`
-	Gender           string `gorm:"size:1"`	
+	Gender           string `gorm:"size:1"`
 	Dob              time.Time
-	Citizenship      string `gorm:"size:45"`
-	BirthCertificate BitBool
-	IdProfile        int
-	Profile          *Profile `gorm:"foreignkey:IdProfile;PRELOAD:false"`
+	Citizenship      string   `gorm:"size:45"`
+	BirthCertificate BitBool  `gorm:"column:birthcertificate"`
+	IdProfile        uint     `gorm:"column:idprofile;not null"`
+	Profile          *Profile `gorm:"foreignkey:idprofile;PRELOAD:false"`
 	IdFamily         int
 	Family           *Family     `gorm:"foreignkey:IdPerson"`
 	UsaHockeys       []UsaHockey `gorm:"foreignkey:IdPerson"`
+
+	// Controll information
+	ID        uint      `gorm:"column:idperson;primary_key"`
+	IsActive  bool      `gorm:"column:isactive"`
+	UpdatedAt time.Time `gorm:"column:updated"`
+	//	DeletedAt  *time.Time `sql:"index"`
+	CreatedAt time.Time
 }
 
 //
