5c5c0f07ed6d3ab5c8f1bf3051d341afb433cb60
David Bigelow <david.bigelow@pdgc.com>
2020-03-30 08:24:35 -0700

    Adding scoring to the LiveGame

--- Livegame.go
+++ Livegame.go
@@ -21,6 +21,9 @@ type Livegame struct {
 	HomeShotsOnGoal Sog `gorm:"foreignkey:IdLiveGame,IdTeamHome;association_foreignkey:IdLiveGame,IdTeam"`
 	AwayShotsOnGoal Sog `gorm:"foreignkey:IdLiveGame,IdTeamAway;association_foreignkey:IdLiveGame,IdTeam"`
 
+	HomeScoring Scoring `gorm:"foreignkey:IdLiveGame,IdTeamHome;association_foreignkey:IdLiveGame,IdTeam"`
+	AwayScoring Scoring `gorm:"foreignkey:IdLiveGame,IdTeamAway;association_foreignkey:IdLiveGame,IdTeam"`
+
 	Idschedule uint
 	Typetag    string
 	Statetag   string
--- Scoring.go
+++ Scoring.go
@@ -1,1 +1,27 @@
 package entity
+
+import (
+	"time"
+)
+
+type Scoring struct {
+
+	IdLiveGame uint `gorm:"column:idlivegame"`
+	IdTeam uint `gorm:"column:idteam"`
+
+	IdRosterGoal uint `gorm:"column:idroster_goal"`
+	IdRosterA1 uint `gorm:"column:idroster_a1"`
+	IdRosterA2 uint `gorm:"column:idroster_a2"`
+
+	GoalType string `gorm:"column:goaltype"`
+	Period   uint
+	TimeScored string `gorm:"column:timescored"`
+
+	// Controll information
+	ID uint `gorm:"column:idscoring;primary_key"`
+	IsActive bool    `gorm:"column:isactive"`
+	UpdatedAt  time.Time `gorm:"column:updated"`
+	//	DeletedAt  *time.Time `sql:"index"`
+	CreatedAt  time.Time
+
+}
