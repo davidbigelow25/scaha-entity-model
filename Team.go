8c565fe5a08d2ff003f35acec52a38c1eda70df0
David Bigelow <david.bigelow@pdgc.com>
2020-03-31 08:51:34 -0700

    isExhibition not quite right

--- Team.go
+++ Team.go
@@ -3,24 +3,23 @@ package entity
 import "time"
 
 type Teams struct {
+	IdClub uint `gorm:"column:idclub"`
 
-	IdClub        uint    `gorm:"column:idclub"`
-
-	Name          string  `gorm:"column:sname"`
-	TeamName      string  `gorm:"column:teamname"`
-	TeamGender    string  `gorm:"column:teamgender"`
-	IdSkillLevel  uint    `gorm:"column:IdSkillLevel"`
-	IdDivisions   uint    `gorm:"column:iddivisions"`
-	DivisionTag   string  `gorm:"column:divisiontag"`
-	SkillLevelTag string  `gorm:"column:skillleveltag"`
-	IsExhibition  BitBool `gorm:"column:isexhibition"`
+	Name          string `gorm:"column:sname"`
+	TeamName      string `gorm:"column:teamname"`
+	TeamGender    string `gorm:"column:teamgender"`
+	IdSkillLevel  uint   `gorm:"column:IdSkillLevel"`
+	IdDivisions   uint   `gorm:"column:iddivisions"`
+	DivisionTag   string `gorm:"column:divisiontag"`
+	SkillLevelTag string `gorm:"column:skillleveltag"`
+	IsExhibition  bool   `gorm:"column:isexhibition"`
 	Year          uint
 	SeasonTag     string `gorm:"column:seasontag"`
 	ScheduleTags  string `gorm:"column:scheduletags"`
 	GroupTag      string `gorm:"column:grouptag"`
 
 	//  Rosters and List of Suspensions
-	RosterSpots []*Roster `gorm:"foreignkey:IdTeam"`
+	RosterSpots []*Roster      `gorm:"foreignkey:IdTeam"`
 	Suspensions []*Suspensions `gorm:"foreignkey:IdTeam"`
 
 	// Controll information
